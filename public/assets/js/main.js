d696749ca3d00645dadee9f864b67514dd736337
Julien Maze <mazejul@gmail.com>
2015-01-25 17:26:08 +0000

    Now with more pyramids

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,22 +1,54 @@
 (function() {
-    // Matter.js module aliases
     var Engine = Matter.Engine,
         World = Matter.World,
-        Bodies = Matter.Bodies;
+        Bodies = Matter.Bodies,
+        Composites = Matter.Composites
+        MouseConstraint = Matter.MouseConstraint;
+    
+    var _engine,
+        _mouseConstraint,
+        _sceneEvents = [];
 
-    // create a Matter.js engine
-    var engine = Engine.create(document.body);
+    var Test = {};
 
-    // create two boxes and a ground
-    var boxA = Bodies.rectangle(400, 200, 80, 80);
-    var boxB = Bodies.rectangle(450, 50, 80, 80);
-    var ground = Bodies.rectangle(400, 610, 810, 60, {
-        isStatic: true
-    });
+    Test.init = function() {
+    	_engine = Engine.create(document.body);
 
-    // add all of the bodies to the world
-    World.add(engine.world, [boxA, boxB, ground]);
+    	var offset = 25;
+        var ground = Bodies.rectangle(400, -offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
+           ceiling = Bodies.rectangle(400, 600 + offset, 800.5 + 2 * offset, 50.5, { isStatic: true }),
+          leftWall = Bodies.rectangle(-offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true }),
+         rightWall = Bodies.rectangle(800 + offset, 300, 50.5, 600.5 + 2 * offset, { isStatic: true });
 
-    // run the engine
-    Engine.run(engine);
+        // add a mouse controlled constraint
+        _mouseConstraint = MouseConstraint.create(_engine);
+        World.add(_engine.world, _mouseConstraint);
+
+        // add all of the bodies to the world
+        World.add(_engine.world, [ground, ceiling, leftWall, rightWall]);
+
+        // run the engine
+        Engine.run(_engine);
+
+        Test.pyramid();
+    };
+
+    // call init when the page has loaded fully
+    if (window.addEventListener) {
+        window.addEventListener('load', Test.init);
+    } else if (window.attachEvent) {
+        window.attachEvent('load', Test.init);
+    }
+
+    Test.pyramid = function() {
+        var _world = _engine.world;
+        
+        var stack = Composites.pyramid(100, 100, 15, 10, 0, 0, function(x, y, column, row) {
+            return Bodies.circle(x, y, 20);
+        });
+        
+        World.add(_world, stack);
+        
+        var renderOptions = _engine.render.options;
+    };
 })();
